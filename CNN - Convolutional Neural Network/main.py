c303a5c652b3db4d10971781ed92356403203559
Nicolas Anderson <nicolas-31@outlook.com>
2018-04-04 19:21:03 +0200

    updated

--- CNN - Convolutional Neural Network/main.py
+++ CNN - Convolutional Neural Network/main.py
@@ -1,40 +1,45 @@
-import numpy as np
-from PIL import Image
 import pickle
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, add
+from keras.layers import Conv2D, MaxPooling2D
+import keras
+import numpy as np
 
-def openFile(filename, skip=True):
-    with open(filename, 'r') as f: #
-        if skip:
-            f.readline()
-        return f.readlines()
+batch_size = 25
+num_classes = 2
+epochs = 10
+(x_train, y_train),(x_test, y_test) = pickle.load(open('female.p', 'rb'))
+#(x_train, y_train),(x_test, y_test) = pickle.load(open('male.p', 'rb'))
 
+def reshape(x):
+    x = np.asarray(x)
+    x = x.astype('float32')
+    return x/255
 
-def getPathName(a, b, c):
-    return 'lfw/' + str(a[b]) + '/' + str(a[b]) + '_' + str(str(c).zfill(4)) + '.jpg'
+x_train = reshape(x_train)
+x_test = reshape(x_test)
+y_train = np.asarray(y_train)
+y_test = np.asarray(y_test)
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
 
-
-def getImgage(path):
-    image = Image.open(path).crop((5, 5, 245, 245)).resize((80, 80))
-    return np.asarray(image, dtype="uint8")
-
-def getDataSet(filename):
-    x, y = ([], [])
-
-    for line in openFile(filename):
-        l = line.split()
-        #if l[0] in open('female_names.txt').read():
-        if l[0] in open('male_names.txt').read():
-            label = 1
-        else:
-            label = 0
-        for i in range(int(l[1])):
-            path = getPathName(l, 0, i+1)
-            x.append(getImgage(path))
-            y.append(label)
-    return x, y
-
-
-x = getDataSet('peopleDevTrain.txt')
-y = getDataSet('peopleDevTest.txt')
-#pickle.dump((x, y), open('female.p', 'wb'))
-pickle.dump((x, y), open('male.p', 'wb'))
+model = Sequential()
+model.add(Conv2D(8, 3, activation='relu', input_shape=np.shape(x_train)[1:]))
+model.add(Conv2D(16, 3, activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(24, 3, activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(32, 3, activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Flatten())
+model.add(Dropout(0.25))
+model.add(Dense(128,activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes, activation='sigmoid'))
+model.summary()
+model.compile(
+        loss=keras.losses.categorical_crossentropy,
+        optimizer=keras.optimizers.Adadelta(),
+        metrics=['accuracy'])
+model.fit(x_train,y_train,batch_size=batch_size,epochs=epochs,verbose=2,validation_data=(x_test,y_test))
+score = model.evaluate(x_test,y_test,verbose=2)
